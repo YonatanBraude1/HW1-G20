49765da3bb5b95a6252555869f4a4c0010bc17e5
Yonatan Sherer <yonatansh103@gmail.com>
2024-07-15 14:19:08 +0300

    InitRepo

---
+++ group 20/public/logout.js
@@ -0,0 +1,8 @@
+ 
+
+// Function for "Login without account" button (just an example)
+function logout() {
+    alert("logout.");
+    // Implement action as needed
+    window.location.href = "./home.html";
+}
